cf2b44833c8261871e7dd56f339f898fae87dd6d
zzinue <zzinue@gmail.com>
2023-09-13 20:57:10 -0600

    password checker

---
+++ main.js
@@ -0,0 +1,40 @@
+const password=document.getElementById('password');
+const message=document.getElementById('message');
+const strength=document.getElementById('strength');
+const submitButton=document.querySelector('button')
+
+password.addEventListener('input',function(){
+
+    const passwordValue=password.value;
+    const passwordLength=passwordValue.length;
+
+
+    let strengthValue='';
+    
+        if(password===0){
+            strengthValue='';
+
+
+        }else if(passwordLength<6){
+            strengthValue="weak"
+        }else if(passwordLength<10){
+            strengthValue="medium"
+        }else{
+strengthValue="strong"
+        }
+
+        strength.textContent=strengthValue;
+        message.style.display='block';   // display the message
+         
+})
+
+submitButton.addEventListener('click', function(){
+    const passwordType=password.getAttribute('type')
+
+    if(passwordType==='password'){
+        password.setAttribute('type','text')
+    }else{
+        password.setAttribute('type','password')
+
+    }
+})
